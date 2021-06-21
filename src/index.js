b43d49500cfd0255011246e22f1cfee20c442b9d
TALEM Idir <idir.talem@gmail.com>
2021-06-21 11:12:10 +0100

    Making up the global style and theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { App } from "./App";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./utils/theme";
+import GlobalStyles from "./utils/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <>
+        <App />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
